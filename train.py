2b11b4f05205a6261ff4c5924d1fdc579c697636
Saksham Singh <mailtosakshamsingh@gmail.com>
2025-05-11 10:54:02 -0700

    train resnet50

--- train.py
+++ train.py
@@ -1,6 +1,6 @@
 from imageai.Classification.Custom import ClassificationModelTrainer
 
 model_trainer = ClassificationModelTrainer()
-model_trainer.setModelTypeAsDenseNet121()
+model_trainer.setModelTypeAsResNet50()
 model_trainer.setDataDirectory(r".")
-model_trainer.trainModel(num_experiments=20, verbose=True)
+model_trainer.trainModel(num_experiments=100, verbose=True)
